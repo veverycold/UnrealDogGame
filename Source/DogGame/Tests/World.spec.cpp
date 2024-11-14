08c92f3b8d3a1f7bab32116b457f693161b31045
Denis <Denis0997@yandex.ru>
2024-11-14 16:47:43 +0300

    lesson 1 - tests

---
+++ Source/DogGame/Tests/World.spec.cpp
@@ -0,0 +1,41 @@
+// Dog Game. Copyright VeveryCold. All Rights Reserved.
+
+#if WITH_AUTOMATION_TESTS
+
+#include "CoreMinimal.h"
+#include "Misc/AutomationTest.h"
+#include "DogGame/Tests/Utils/TestUtils.h"
+#include "DogGame/World/GG_Grid.h"
+#include "Kismet/GameplayStatics.h"
+
+
+BEGIN_DEFINE_SPEC(FDogWorld, "Dog",
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority)
+UWorld* World;
+END_DEFINE_SPEC(FDogWorld)
+
+void FDogWorld::Define()
+{
+    	using namespace LifeExe::Test;
+
+Describe("WorldGid",
+            [this]()
+            {
+                BeforeEach(
+                    [this]()
+                    {
+                        AutomationOpenMap("GameLevel");
+                        World = GetTestGameWorld();
+                    });
+                It("OnlyOneValidGridActorShouldExists",
+                    [this]()
+                    {
+                        TArray<AActor*> Grids;
+                        UGameplayStatics::GetAllActorsOfClass(World, ASG_Grid::StaticClass(), Grids);
+                        TestTrueExpr(Grids.Num() == 1);
+                        TestNotNull("Grid actor exist", Grids[0]);
+                    });
+            });
+}
+
+#endif
